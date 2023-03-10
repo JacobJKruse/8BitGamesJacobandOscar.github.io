cd5b19c62f6533c77cdbfb08c045cf5d374b1047
Jacob Kruse <jakruse02@gmail.com>
2023-03-10 10:32:23 -0600

    catalog changes

--- catalog.js
+++ catalog.js
@@ -103,32 +103,49 @@ function appendData(data) {
     for (let x of mainContainer) {
         if (x.id == "0") {
             
-            x.innerHTML = `<h3>${data[system][0]["PRODUCT_NAME"]}</h3> <br><img src="${data[system][0]["IMG_LINK"]}"> <br> ${data[system][0]["PRODUCT_DESC"]} <br> Price: ${data[system][0]["PRICE"]} <br> Rating: ${data[system][0]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+            x.innerHTML = `<img class="center" width="200px"src="${data[system][0]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][0]["PRODUCT_NAME"]}</h3> <br> ${data[system][0]["PRODUCT_DESC"]} <br> Price: ${data[system][0]["PRICE"]} <br> Rating: ${data[system][0]["RATING"]}%`;
+            }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][0]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][0]["PRODUCT_NAME"]}</h3> <br> ${data[system][0]["PRODUCT_DESC"]} <br> Price: ${data[system][0]["PRICE"]} <br> Rating: ${data[system][0]["RATING"]}%`;
+            }
         }
         else if (x.id == "1") {
             
-            x.innerHTML = `<h3>${data[system][1]["PRODUCT_NAME"]}</h3> <br> ${data[system][1]["PRODUCT_DESC"]} <br> Price: ${data[system][1]["PRICE"]} <br>  Rating: ${data[system][1]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+                x.innerHTML = `<img class="center" width="200px"src="${data[system][1]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][1]["PRODUCT_NAME"]}</h3> <br> ${data[system][1]["PRODUCT_DESC"]} <br> Price: ${data[system][1]["PRICE"]} <br> Rating: ${data[system][1]["RATING"]}%`;
+                }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][1]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][1]["PRODUCT_NAME"]}</h3> <br> ${data[system][1]["PRODUCT_DESC"]} <br> Price: ${data[system][1]["PRICE"]} <br> Rating: ${data[system][1]["RATING"]}%`;
+                }
         }
         else if (x.id == "2") {
             
-            x.innerHTML = `<h3>${data[system][2]["PRODUCT_NAME"]}</h3> <br> ${data[system][2]["PRODUCT_DESC"]} <br> Price: ${data[system][2]["PRICE"]} <br>  Rating: ${data[system][2]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+                x.innerHTML = `<img class="center" width="200px"src="${data[system][2]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][2]["PRODUCT_NAME"]}</h3> <br> ${data[system][2]["PRODUCT_DESC"]} <br> Price: ${data[system][2]["PRICE"]} <br> Rating: ${data[system][2]["RATING"]}%`;
+                }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][2]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][2]["PRODUCT_NAME"]}</h3> <br> ${data[system][2]["PRODUCT_DESC"]} <br> Price: ${data[system][2]["PRICE"]} <br> Rating: ${data[system][2]["RATING"]}%`;
+                }
         }
         else if (x.id == "3") {
             
-            x.innerHTML = `<h3>${data[system][3]["PRODUCT_NAME"]}</h3> <br> ${data[system][3]["PRODUCT_DESC"]} <br> Price: ${data[system][3]["PRICE"]} <br>  Rating: ${data[system][3]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+                x.innerHTML = `<img class="center" width="200px"src="${data[system][3]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][3]["PRODUCT_NAME"]}</h3> <br> ${data[system][3]["PRODUCT_DESC"]} <br> Price: ${data[system][3]["PRICE"]} <br> Rating: ${data[system][3]["RATING"]}%`;
+                }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][3]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][3]["PRODUCT_NAME"]}</h3> <br> ${data[system][3]["PRODUCT_DESC"]} <br> Price: ${data[system][3]["PRICE"]} <br> Rating: ${data[system][3]["RATING"]}%`;
+                }
         }
         else if (x.id == "4") {
-           
-            x.innerHTML = `<h3>${data[system][4]["PRODUCT_NAME"]}</h3> <br> ${data[system][4]["PRODUCT_DESC"]} <br> Price: ${data[system][4]["PRICE"]} <br> Rating: ${data[system][4]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+                x.innerHTML = `<img class="center" width="200px"src="${data[system][4]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][4]["PRODUCT_NAME"]}</h3> <br> ${data[system][4]["PRODUCT_DESC"]} <br> Price: ${data[system][2]["PRICE"]} <br> Rating: ${data[system][4]["RATING"]}%`;
+                }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][4]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][4]["PRODUCT_NAME"]}</h3> <br> ${data[system][4]["PRODUCT_DESC"]} <br> Price: ${data[system][4]["PRICE"]} <br> Rating: ${data[system][4]["RATING"]}%`;
+                }
         }
         else if (x.id == "5") {
-            x.innerHTML = `<h3>${data[system][5]["PRODUCT_NAME"]}</h3> <br> ${data[system][5]["PRODUCT_DESC"]} <br> Price: ${data[system][5]["PRICE"]} <br> Rating: ${data[system][5]["RATING"]}%`;
-            
+            if(system == "SNES" ||system == "N64") {
+                x.innerHTML = `<img class="center" width="200px"src="${data[system][5]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][5]["PRODUCT_NAME"]}</h3> <br> ${data[system][5]["PRODUCT_DESC"]} <br> Price: ${data[system][5]["PRICE"]} <br> Rating: ${data[system][5]["RATING"]}%`;
+                }else{
+            x.innerHTML = `<img class="center" height="200px"src="${data[system][5]["IMG_LINK"]}"> <br><h3 style="text-align:center;font-weight: bold;">${data[system][5]["PRODUCT_NAME"]}</h3> <br> ${data[system][5]["PRODUCT_DESC"]} <br> Price: ${data[system][5]["PRICE"]} <br> Rating: ${data[system][5]["RATING"]}%`;
+                }
         }
 
     }
