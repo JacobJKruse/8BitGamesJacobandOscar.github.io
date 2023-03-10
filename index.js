3eb0a103f6457c4815d620a479fa77d0a09964cf
Oscar-Lenkaitis <oilenkaitis@gmail.com>
2023-03-10 00:12:22 -0600

    switch

--- index.js
+++ index.js
@@ -1,142 +1,50 @@
-var system = "NES";
+function updateIndex(){
+  fetch('indexInfo.json')
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data) {
+                getReviews(data);
+              })
+              .catch(function (err) {
+                console.log('error:' + err);
+              })
+  }
 
-var nesbutton = document.getElementById("NES");
-nesbutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    
-    console.log("button pressed");
-    system = "NES";
-    update();
+            function getReviews(data) {
+              let mainContainer = document.getElementById("myData");
+              let div = document.createElement("div");
+              div.innerHTML = ` <h2><img class="bd-placeholder-img rounded-circle"width="140" height="140" src=${data.REVIEWS.MAIN[0].IMG} ><br> ${data.REVIEWS.MAIN[0].NAME} <br> </h2> <p>${data.REVIEWS.MAIN[0].REVIEW} <br> -${data.REVIEWS.MAIN[0].DATE}</p> `;
+              mainContainer.appendChild(div);
+              let mainContainer2 = document.getElementById("myData2");
+              let div2 = document.createElement("div");
+              div2.innerHTML = `<h2><img class="bd-placeholder-img rounded-circle" src=${data.REVIEWS.MAIN[1].IMG} width="140" height="140"> <br> ${data.REVIEWS.MAIN[1].NAME}</h2><p>${data.REVIEWS.MAIN[1].REVIEW} <br> -${data.REVIEWS.MAIN[1].DATE}</p>`;
+              mainContainer2.appendChild(div2);
+              let mainContainer3 = document.getElementById("myData3");
+              let div3 = document.createElement("div");
+              div3.innerHTML = `<h2><img class="bd-placeholder-img rounded-circle" src=${data.REVIEWS.MAIN[2].IMG} width="140" height="140"><br>  ${data.REVIEWS.MAIN[2].NAME}</h2><p>${data.REVIEWS.MAIN[2].REVIEW}  <br> -${data.REVIEWS.MAIN[2].DATE}</p>`;
+              mainContainer3.appendChild(div3);
+  
+              let mainContainerAd1 = document.getElementById("AD1");
+              let divAD1 = document.createElement('div');
+              divAD1.innerHTML = `<h1 class="featurette-heading fw-bold lh-1">${data.ADS.MAIN[0].TITLE}<h1><p style="lead">${data.ADS.MAIN[0].DESC}</p>`;
+              mainContainerAd1.appendChild(divAD1);
+  
+              let mainContainerAd1IMG = document.getElementById("AD1IMG");
+              let divAD1IMG = document.createElement('div');
+              divAD1IMG.innerHTML = `<img height="300px" width="300px"src="${data.ADS.MAIN[0].IMG}">`;
+              mainContainerAd1IMG.appendChild(divAD1IMG);
+  
+              let mainContainerAd2 = document.getElementById("AD2");
+              let divAD2 = document.createElement('div');
+              divAD2.innerHTML = `<h1 class="featurette-heading fw-bold lh-1">${data.ADS.MAIN[1].TITLE}<h1><p style="lead">${data.ADS.MAIN[1].DESC}</p>`;
+              mainContainerAd2.appendChild(divAD2);
+  
+              let mainContainerAd2IMG = document.getElementById("AD2IMG");
+              let divAD2IMG = document.createElement('div');
+              divAD2IMG.innerHTML = `<img height="300px"  width="300px"src="${data.ADS.MAIN[1].IMG}">`;
+              mainContainerAd2IMG.appendChild(divAD2IMG);
+  
+            }
 
-});
-
-var snesbutton = document.getElementById("SNES");
-snesbutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "SNES";
-    update();
-
-});
-
-var n64button = document.getElementById("N64");
-n64button.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "N64";
-    update();
-
-});
-
-var gcbutton = document.getElementById("GC");
-gcbutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "GC";
-    update();
-
-});
-
-var wiibutton = document.getElementById("WII");
-wiibutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "WII";
-    update();
-
-});
-
-var gbbutton = document.getElementById("GB");
-gbbutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "GB";
-    update();
-
-});
-
-var gbabutton = document.getElementById("GBA");
-gbabutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "GBA";
-    update();
-
-});
-
-var dsbutton = document.getElementById("DS");
-dsbutton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "DS";
-    update();
-
-});
-
-var ds3button = document.getElementById("3DS");
-ds3button.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button pressed");
-    system = "3DS";
-    update();
-
-});
-
-function update() {
-    fetch('data.json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            appendData(data);
-        })
-        .catch(function (err) {
-            console.log('error:' + err);
-        })
-}
-
-
-
-function appendData(data) {
-
-    let mainContainer = document.getElementsByTagName("p");
-    for (let x of mainContainer) {
-        if (x.id == "0") {
-            
-            x.innerHTML = `${data[system][0]["PRODUCT_NAME"]} <br> Price: ${data[system][0]["PRICE"]} <br> Rating: ${data[system][0]["RATING"]}%`;
-            
-        }
-        else if (x.id == "1") {
-            
-            x.innerHTML = `${data[system][1]["PRODUCT_NAME"]} <br> Price: ${data[system][1]["PRICE"]} <br>  Rating: ${data[system][1]["RATING"]}%`;
-            
-        }
-        else if (x.id == "2") {
-            
-            x.innerHTML = `${data[system][2]["PRODUCT_NAME"]} <br> Price: ${data[system][2]["PRICE"]} <br>  Rating: ${data[system][2]["RATING"]}%`;
-            
-        }
-        else if (x.id == "3") {
-            
-            x.innerHTML = `${data[system][3]["PRODUCT_NAME"]} <br> Price: ${data[system][3]["PRICE"]} <br>  Rating: ${data[system][3]["RATING"]}%`;
-            
-        }
-        else if (x.id == "4") {
-           
-            x.innerHTML = `${data[system][4]["PRODUCT_NAME"]} <br> Price: ${data[system][4]["PRICE"]} <br> Rating: ${data[system][4]["RATING"]}%`;
-            
-        }
-        else if (x.id == "5") {
-            x.innerHTML = `${data[system][5]["PRODUCT_NAME"]} <br> Price: ${data[system][5]["PRICE"]} <br> Rating: ${data[system][5]["RATING"]}%`;
-            
-        }
-
-    }
-}
-
-
-update();
-
-
-
-
+  updateIndex();
